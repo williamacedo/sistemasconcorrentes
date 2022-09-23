1b6cdc007e0c50f5302b964504d11e36950cb60c
William de Macedo <williamcedo37@yahoo.com.br>
2022-09-23 09:19:35 -0300

    Add files via upload

--- com_1/sc/Servidor.java
+++ com_1/sc/Servidor.java
@@ -3,12 +3,14 @@ package com_1.sc;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Servidor {
     public static void main(String[] args) {
         ServerSocket servidor = null;
+        Scanner scanner = null;
         try {
-            System.out.println("[SERVER] - Iniciando Servidor na porta 1234....");
+            System.out.println("[SERVER] - Iniciando Servidor na porta 8000....");
             servidor = new ServerSocket(8000);
             System.out.println("[SERVER] - Servidor iniciado");
 
@@ -16,6 +18,10 @@ public class Servidor {
                 System.out.println("Server - Aguardando conexão.");
                 Socket cliente = servidor.accept();
                 System.out.println("[SERVER] - Conexão recebida de " + cliente.getInetAddress().getHostAddress());
+                scanner = new Scanner(cliente.getInputStream());
+                while (scanner.hasNextLine()) {
+                    System.out.println("[PROCESSO] - Mensagem Recebida: " + scanner.nextLine());
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
